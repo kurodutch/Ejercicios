413a2c51031fc262eecd228123dd758cea00389f
kurodutch <34357575+kurodutch@users.noreply.github.com>
2024-06-02 16:50:05 -0400

    Ejercicio 1.14 paso 3 terminado

--- 1.14/src/App.jsx
+++ 1.14/src/App.jsx
@@ -6,6 +6,11 @@ const Button = (props) => {
   )
 }
 
+const Display = (props) => {
+  return(
+    <div> {props.text} has {props.votes} votes </div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -18,37 +23,79 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  //const points = Array(8).fill(0)//
 
-  const points = [0,0,0,0,0,0,0,0]
-  let copy = [...points]
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-  
+  const [votos, setVotos] = useState(Array(anecdotes.length).fill(0))
+  const [anecdoteIndex, setAnecdoteIndex] = useState(0)
+  const [maximo, setMax] = useState(0)
+  const [anecdotaMayor, setAnecdotaMayor] = useState('')
   const largo = anecdotes.length
-  //let copy1 = []
+  let indice = 0
+  let max_aux = 1
+  let nuevoMax = 0
+  
 
-  const manejarBoton  = () => {
-    console.log('copy selected tiene', copy[selected])
-    console.log('array copy tiene', copy)
-    copy[selected] = copy[selected]+1
-    return(
-      copy
-    )
-  }
 
   function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
   }
+
+  const siguienteClick = () => {
+    console.log('anecdoteIndex antes de: ', anecdoteIndex)
+    indice =(random(0, anecdotes.length-1)) // 5 por ejemplo
+    setSelected(indice) //a [anecdotes] se le va el indice 5 para mostrar
+    console.log('anecotedeindex luego de: ', indice)
+  }
   
+  const botonVotar = () => {
+    //console.log('el indice actual es: ', selected)
+    //console.log('votos antes: ', votos)
+    const copyVotos = [...votos]
+    //console.log('copia de votos antes de añadir 1: ', copyVotos)
+    //console.log('el indice actual es: ', selected)
+    copyVotos[selected] +=1
+    //console.log('copia de votos luego de añadir 1: ', copyVotos)
+    //console.log('el arreglo votos tiene actualmente: ', votos)
+    if (copyVotos[selected] > maximo) {
+      console.log('es mayor que', max_aux)
+      console.log('valor de max antes de actualizar: ', max_aux)
+      nuevoMax = max_aux+1
+      nuevoMax = copyVotos[selected]
+      console.log('valor de max luego de actualizar: ', nuevoMax)
+      setMax(nuevoMax)
+      setAnecdotaMayor (anecdotes[selected])
+    }
+    setVotos(copyVotos)
+    return(
+      nuevoMax
+    )
+    //console.log('Votos luego de: ', copyVotos)
+   // console.log('el arreglo votos tiene actualmente: ', votos)
+    /*for (let i = 0; i < votos.length; i++){
+      console.log('dentro del for')
+      console.log(votos)
+      console.log('votos[i] es' ,  votos[i])
+      if (votos[i] > max) {
+        anecdotaMayor = votos[anecdotes]
+        max = votos[i]
+        console.log('dentro del IF')
+      }
+    }*/
+    
+
+  }
   return (
     <div>
-      {anecdotes[selected]}
+      <div>
+      <Display text = {anecdotes[selected]} votes = {votos[selected]}/> 
+      </div>
       <br></br>
-      <Button handleClick = {manejarBoton} text ='votar1'/>
-      <Button handleClick = {() => setSelected(random(0,anecdotes.length-1))} text='muestrame otra anecdota' />
-      <Button handleClick = {() => setSelected(3)} text='muestrame otra anecdota2' />
-      {copy[selected]}
+      <Button handleClick = {botonVotar} text ='votar'/>
+      <Button handleClick = {siguienteClick} text='muestrame otra anecdota' />
+      <h2>Anecodta con más votos</h2>
+      <div>
+        <Display text = {anecdotaMayor} votes = {maximo}/>
+      </div>
     </div>
   )
 }
